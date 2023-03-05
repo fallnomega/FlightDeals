15efb7b41584cdb92ca672b399221d452a6c2d83
fallnomega <fallnomega@gmail.com>
2023-03-05 10:00:48 -0800

    starting work on flight search api

--- flight_search.py
+++ flight_search.py
@@ -1,5 +1,35 @@
 import requests
+import os
+import datetime
 
+
+# This class is responsible for talking to the Flight Search API.
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
+    def __init__(self):
+        # API doc - https://tequila.kiwi.com/portal/docs/tequila_api/search_api
+
+        self.endpoint = os.environ.get('KIWI_URL')
+        self.apikey = os.environ.get('KIWI_KEY')
+        self.fly_from = 'OAK,SFO'
+        self.fly_to = ''
+        self.date_from = ''
+        self.date_to = ''
+        self.return_from = ''
+        self.return_to = ''
+        self.adults = 2
+        self.children = 2
+        self.selected_cabins = ''
+        self.adult_hold_bag = ''
+        self.adult_hand_bag = ''
+        self.child_hold_bag = ''
+        self.child_hand_bag = ''
+        self.curr = 'USD'
+        self.price_to = 4000
+
+    def get_flight_info(self):
+        params = {'apikey':self.apikey,'fly_from': self.fly_from, 'fly_to': self.fly_to, 'date_from': self.date_from,
+                  'date_to': self.date_to, 'adults': self.adults, 'children': self.children}
+
     pass
+
+# flightdealfinder
