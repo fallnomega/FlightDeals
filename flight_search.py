6541a1e16b47f282178183fd39b047b4a27eea86
fallnomega <fallnomega@gmail.com>
2023-03-06 08:45:09 -0800

    work on flight search api

--- flight_search.py
+++ flight_search.py
@@ -12,10 +12,11 @@ class FlightSearch:
         self.apikey = os.environ.get('KIWI_KEY')
         self.fly_from = 'SFO'
         self.fly_to = 'BER'
-        self.date_from = '04/04/2023'
-        self.date_to = '05/04/2023'
-        self.return_from = '06/04/2023'
-        self.return_to = '07/04/2023'
+        self.date_from = (datetime.datetime.today() + datetime.timedelta(days=1)).strftime('%d/%m/%Y')
+        self.date_to = (datetime.datetime.today() + datetime.timedelta(weeks=24)).strftime('%d/%m/%Y')
+        #
+        # self.return_from = (datetime.datetime.today() + datetime.timedelta(days=7)).strftime('%m%d%Y')
+        # self.return_to = (datetime.datetime.today() + datetime.timedelta(weeks=25)).strftime('%m%d%Y')
         self.adults = 2
         self.children = 2
         self.selected_cabins = 'C'
@@ -27,22 +28,25 @@ class FlightSearch:
         self.currency = 'USD'
         self.price_to = 4000
         self.vehicle_type = 'aircraft'
+        self.flight_type = 'oneway'
 
     def get_flight_info(self):
-        params = {'apikey': self.apikey, 'fly_from': self.fly_from, 'fly_to': self.fly_to, 'date_from': self.date_from,
-                  'date_to': self.date_to}  # , 'adults': self.adults, 'children': self.children
+        parameters = {'fly_from': self.fly_from, 'fly_to': self.fly_to, 'date_from': self.date_from,
+                  'date_to': self.date_to, 'max_fly_duration': 20,
+                  'flight_type': self.flight_type, 'one_for_city': 0, 'one_per_date': 0, 'adults': self.adults,
+                  'children': self.children, 'selected_cabins': self.selected_cabins,
+                  'mix_with_cabins': self.mix_with_cabins,
+                  'adult_hold_bag': self.adult_hold_bag, 'adult_hand_bag': self.adult_hand_bag,
+                  'child_hold_bag': self.child_hold_bag, 'child_hand_bag': self.child_hand_bag,
+                  'partner_market': 'us', 'curr': self.currency, 'max_stopovers': 2, 'max_sector_stopovers': 2,
+                  'vehicle_type': self.vehicle_type, 'limit': 20}
         api = '/v2/search'
-        headerz = {'accept': 'application/json','apikey': self.apikey}
+        headerz = {'accept': 'application/json', 'apikey': self.apikey}
         # requestor = requests.get(url=f"{self.endpoint}{api}",params=params)
-        requestor = requests.get( headers=headerz,
-            url=f'https://api.tequila.kiwi.com/v2/search?fly_from={self.fly_from}&fly_to={self.fly_to}'
-                f'&date_from={self.date_from}&date_to={self.date_to}&return_from={self.return_from}'
-                f'&return_to={self.return_to}&flight_type=round&adults={self.adults}&children={self.children}'
-                f'&selected_cabins={self.selected_cabins}&mix_with_cabins={self.mix_with_cabins}'
-                f'&adult_hold_bag={self.adult_hold_bag}&adult_hand_bag={self.adult_hand_bag}'
-                f'&child_hold_bag={self.child_hold_bag}&child_hand_bag={self.child_hand_bag}'
-                f'&partner_market=us&curr=USD&max_stopovers=2&max_sector_stopovers=2&vehicle_type=aircraft&limit=500' \
-)
+        # 09%2F07%2F2023
+        requestor = requests.get(headers=headerz, params=parameters,
+                                 url=f'{self.endpoint}{api}'
+                                 )
         print(requestor.url)
         requestor.raise_for_status()
         print(requestor.text)
